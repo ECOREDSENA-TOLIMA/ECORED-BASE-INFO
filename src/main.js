b977211ecede1a307580a26c50ae5d6e15c8f597
Mao <mao8a87@gmail.com>
2021-05-25 18:59:23 -0500

    new v is ready

--- src/main.js
+++ src/main.js
@@ -6,7 +6,21 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './styles/_styles.sass'
-import '@/mixins/globalMixins'
+
+import config from './config/global'
+Vue.prototype.$config = config
+
+const packageJson = require('../package.json')
+Vue.prototype.$package = packageJson
+
+Vue.component('Creditos', () =>
+  import(
+    '../node_modules/ecored-base-pkg/src/components/plantilla/CreditosComp.vue'
+  ),
+)
+Vue.component('Footer', () =>
+  import('../node_modules/ecored-base-pkg/src/components/plantilla/Footer.vue'),
+)
 
 Vue.config.productionTip = false
 
